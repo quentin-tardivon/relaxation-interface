2b3f4daa8a4f8e02a3d5f63987708a7a8adc3a32
Quentin Tardivon <quentin.tardivon@gmail.com>
2017-11-16 21:34:58 +0000

    Fullscreen api

--- script.js
+++ script.js
@@ -33,3 +33,25 @@ function dragMoveListener (event) {
 
 // this is used later in the resizing and gesture demos
 window.dragMoveListener = dragMoveListener;
+
+function toggleFullScreen() {
+  var doc = window.document;
+  var docEl = doc.documentElement;
+
+  var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+  var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+  if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+    requestFullScreen.call(docEl);
+  }
+  else {
+    cancelFullScreen.call(doc);
+  }
+}
+
+interact('.tap-target')
+.on('doubletap', function (event) {
+  toggleFullScreen()
+  event.preventDefault();
+})
+
